cdfa1b52cb672785e3ea0132ddeb3185b51db615
Shamhadov <adwedelf@gmail.com>
2024-05-26 18:57:48 +0700

    Comment to commit

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://tgoaty-backend.nomoredomainswork.ru/api'
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/users/me`,
+}
